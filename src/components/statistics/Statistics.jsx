687c669b02163f8b69a1e3b87198ea85818e3d6d
Serdiuk-Serhii <114324858+Serdiuk-Serhii@users.noreply.github.com>
2023-05-01 21:34:41 +0300

    statistics title &&

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title.length > 0 && <h2 className={css.title}>{title}</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(stat => {
